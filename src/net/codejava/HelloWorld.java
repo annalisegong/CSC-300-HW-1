c032e1f8b6cb10788f328ffe036b58134956042e
annalisegong <annalise.gong@cuw.edu>
2022-09-02 13:10:58 -0500

    least func
    attempted to assign top five array values into a tempFive array.
    
    tried to add function that would bubble sort tempFive w [0] containing the smallest value

--- src/net/codejava/HelloWorld.java
+++ src/net/codejava/HelloWorld.java
@@ -5,7 +5,7 @@ public class HelloWorld
 
 	public static void main(String[] args)
 	{
-		for(int i = 0; i <= 4; i++)
+		/*for(int i = 0; i <= 4; i++)
 		{
 			System.out.println("Enter name of salesman: ");
 			Salesman.defineSalesman(System.in());
@@ -16,24 +16,48 @@ public class HelloWorld
 		{
 			topSales[i].name = "";
 			topSales[i].salesTotal = -1;
-		}
-		
-	}
+		}*/
 	
 		int intArray[];
 		intArray = new int[] {2, 3, 8, 10, 5, 7, 1, 6, 9, 4};
-		int max = FindMax(intArray);
-		System.out.println("the largest value in the array list is " + max);
+		int topFive[];
+		topFive = new int[] {-1, -1, -1, -1, -1};
+		int least = findLeast(intArray);
+		//int max = findMax(intArray);
+		//System.out.println("the largest value in the array list is " + max);
 	}
 	
-	protected static int FindMax(int[] array)
+	protected static int findMax(int[] array)
 	{
 		int tempMax = -1;
 		for(int i = 0; i < array.length; i++)
 		{
 			if(tempMax < array[i])
 			{
-				tempMax = array[i]);
+				tempMax = array[i];
 			}
+		return tempMax;
 		}
 	}
+	
+	protected static int findLeast(int[] array)
+	{
+		int tempFive[];
+		tempFive = new int[] {-1, -1, -1, -1, -1};
+		for(int i = 0; i < array.length; i++)
+		{
+			if(array[i] > tempFive[i])
+			{
+				tempFive[i] = array[i];
+			}
+		}
+		for(int a = array.length - 1; a >= 0; a--)
+		{
+			if(tempFive[a] < tempFive[a-1])
+			{
+				tempFive[a-1] = tempFive[a];
+			}
+		}
+		return tempFive[0];
+	}
+}
